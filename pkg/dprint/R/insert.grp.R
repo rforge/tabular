24089ab804dd312c250ad64145ff8ba6698518fc
c_brickner <c_brickner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-18 19:36:19 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tabular@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dprint/R/insert.grp.R
+++ pkg/dprint/R/insert.grp.R
@@ -1,3 +1,82 @@
+#' Insert Values
+#'
+#' Inserts a value into record in tbl.struct objects to control for when empty rows should be inserted into the data structure.  NA's will be suppressed during presentation of the table.
+#'
+#' @param obj vector or data frame
+#' @param dx Index where NA is to be inserted
+#' @param lblescp Label Escape, boolean
+#' @param group indicates that this object is related to the grouping vector and the insetions must go after
+#' @param val
+#' @param dx.up Bolean, If true adjust an index
+#' @export
+insert.grp1 <-
+function(obj,      # vector or data frame
+                      dx,         # Index where NA is to be inserted
+                      lblescp,    # Label Escape, boolean
+                      group=F,  # indicates that this object is related to the grouping vector and the insetions must go after
+                      val=NA,
+                      dx.up =FALSE # If true adjust an index
+)
+{
+  # This section handles data frames
+  if (is.data.frame(obj))
+    {
+      for (i in 1:length(dx))
+        {
+          if (!lblescp[i])
+            { if(i==1)  {obj <- rbind(rep(val, ncol(obj)), obj)}
+              else
+                { # Exception to handle object changing class when subsetting
+                  data1 <- obj[1:(dx[i]-1), ]
+                  data2 <- obj[(dx[i]):nrow(obj), ]
+                  if (!is.data.frame(data1)) {data1 <- as.data.frame(data1); colnames(data1) <- "a1";}
+                  if (!is.data.frame(data2)) {data2 <- as.data.frame(data2); colnames(data2) <- "a1";}
+                   obj <- rbind(data1, rep(val, ncol(obj)), data2)
+                }
+              dx <- dx + 1
+            }
+        }
+    }
+  else if(dx.up)
+    { obj2 <- obj
+      for (dx.i in 1:length(dx))
+        {
+          if (!lblescp[i])
+            {          obj2[obj>=dx[dx.i]] <- obj2[obj>=dx[dx.i]] +1}
+        }
+      obj<-obj2
+    }
+  # This section handles vectors
+  else
+    { # This section handles vectors with the exception of groups
+      if (!group)
+        {
+          for (i in 1:length(dx))
+            {
+              if (!lblescp[i])
+               {
+                  if(i==1) {obj <- c(val, obj)}
+                  else {obj <- c(obj[1:(dx[i]-1)], val, obj[(dx[i]):length(obj)])}
+                  dx <- dx + 1
+                }
+            }
+        }
+      else # This section handles the group vector, index is slightly altered to put NA after the group name
+        {
+          for (i in 1:length(dx))
+            {
+              if (!lblescp[i])
+               {
+                  if(i==length(dx)) {obj <- c(obj, val)}
+                  else {obj <- c(obj[1:(dx[i+1]-1)], val, obj[(dx[i+1]):length(obj)])}
+                  dx <- dx + 1
+               }
+            }
+        }
+    }
+  return(obj)
+}
+
 #' Insert Values Table Structure
 #'
 #' Dispatcher for insert.grp1. Inserts NA positions for tbl, label, and group structure from tbl.obj.
