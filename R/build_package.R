f1bfae1951c2daf75de79f1af0279feaedd6a253
c_brickner <c_brickner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-05 00:42:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tabular@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/build_package.R
+++ R/build_package.R
@@ -1,4 +1,6 @@
-setwd("C:/Users/carlin/Documents/r_dev/r_forge/tabular/pkg")
+#setwd("C:/Users/carlin/Documents/r_dev/r_forge/tabular/pkg")
+setwd("C:/r_dev/r_forge/tabulaR/pkg")
+
 library(devtools)
 library(testthat)
 library(roxygen2)
@@ -7,10 +9,15 @@ has_devel()
 load_all("dprint")
 
 check("dprint")
+
 check_doc("dprint")
+
 run_examples("dprint")
 
-build(pkg = "dprint", path = "C:/VNSNY/dprint", binary = TRUE)
+build(pkg = "dprint",
+      #path = "C:/VNSNY/dprint",
+      path="C:/r_dev/zipped",
+      binary = TRUE)
 
 document("dprint")
 run_examples("dprint")
