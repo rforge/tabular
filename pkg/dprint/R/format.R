ac2ccfbd7329ca7e99244c0e11be10716c84d182
c_brickner <c_brickner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-07 18:58:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tabular@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dprint/R/format.R
+++ pkg/dprint/R/format.R
@@ -15,7 +15,7 @@ function (prop=NULL, r=NULL, n=NULL, digits.p=4, nsmall.p=2, digits.n=6)
 
     if (!is.null(n) & !is.null(r))
       {f <- paste(format(round(100*r/n, 2), digits=digits.p, nsmall=nsmall.p), "% (", format(r, digits=digits.n, nsmall=0), ")", sep="")}
-    else if (!is.null(n))
+    else if (!is.null(prop) & !is.null(r)) 
       {f <- paste(format(round(100*prop, 2), digits=digits.p, nsmall=nsmall.p), "% (", format(r, digits=digits.n, nsmall=0), ")", sep="")}
     else
       {{f <- paste(format(round(100*prop, 2), digits=digits.p, nsmall=nsmall.p), "% ", sep="")}}
